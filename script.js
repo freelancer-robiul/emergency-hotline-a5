7c0caddbcdd918bb9a204e1e636da76003036f30
Robiul Islam <robi.kushtia@gmail.com>
2025-08-28 00:09:38 +0600

    Heart counted, alert message with service name and number, call history card added

--- script.js
+++ script.js
@@ -11,3 +11,52 @@ getElement("card-section").addEventListener("click", function (e) {
     getElement("heart-1").innerText = heart1Number + 1;
   }
 });
+
+getElement("card-section").addEventListener("click", function (e) {
+  if (e.target.classList.contains("btn-call")) {
+    let callButton = e.target;
+    let serviceName = callButton.parentNode.children[1].innerText;
+    let serviceNumber = callButton.parentNode.children[3].innerText;
+
+    let callHistoryContainer = getElement("call-history-container");
+
+    let newDivElement = document.createElement("div");
+
+    newDivElement.innerHTML = `
+    <div
+              class="single-container flex justify-between items-center bg-[#FAFAFA] xl:p-[10px] lg:p-[6px] md:p-[6px] p-[10px] rounded-lg mb-[16px]"
+              >
+              <div class="left-side">
+                <h1
+                  class="2xl:text-[16px] xl:text-[14px] lg:text-[12px] text-[12px] text-[#111111] font-[600] font-inter"
+                >
+                  ${serviceName}
+                </h1>
+                <span
+                  class="xl:text-[14px] lg:text-[12px] md:text-[14px] text-[12px] text-[#5C5C5C] font-[400] font-hind-madurai"
+                  >${serviceNumber}</span
+                >
+              </div>
+              <div class="right-side">
+                <div class="current-date">
+                  <span
+                    class="2xl:text-[16px] xl:text-[14px] lg:text-[12px] text-[12px] text-[#5C5C5C] font-[400] font-hind-madurai"
+                    >11:36:58AM</span
+                  >
+                </div>
+              </div>
+            </div>
+    
+    `;
+
+    let coinNumber = Number(getElement("coin-number").innerText);
+
+    if (coinNumber <= 0) {
+      return alert("Insufficient Coin");
+    } else {
+      alert(`${serviceName}: ${serviceNumber}`);
+      getElement("coin-number").innerText = coinNumber - 20;
+      callHistoryContainer.append(newDivElement);
+    }
+  }
+});
